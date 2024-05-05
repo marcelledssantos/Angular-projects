9c1edb78cbdcc587c5e64f0af071fa40ef5abe74
marcelledssantos <marcelledossantos2@gmail.com>
2024-05-05 12:38:52 -0300

    config

---
+++ Projeto 01- Weather-app/src/app/modules/wheater/components/weather-card/weather-card.component.ts
@@ -0,0 +1,19 @@
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from './../../../../models/interfaces/WeatherDatas';
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+  @Input() WeatherDatasInput!: WeatherDatas;
+// Dados da previsão do tempo recebidos pelo componente pai
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+  }
+
