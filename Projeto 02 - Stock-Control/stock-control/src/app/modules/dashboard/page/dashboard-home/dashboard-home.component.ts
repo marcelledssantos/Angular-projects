63941f302be5969c0124f0317f4042352a3d0e2e
marcelledssantos <marcelledossantos2@gmail.com>
2024-05-17 00:37:05 -0300

    implementando gráfico de produtos em estoque

--- Projeto 02 - Stock-Control/stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ Projeto 02 - Stock-Control/stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,3 +1,4 @@
+import { ChartData, ChartOptions } from 'chart.js';
 import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
@@ -14,6 +15,9 @@ export class DashboardHomeComponent implements OnInit, OnDestroy{
   private destroy$ = new Subject<void>();
   public productsList: Array<GetAllProductsResponse> = [];
 
+  public productsChartDatas!: ChartData;
+  public productsChartOptions!: ChartOptions;
+
   constructor(
     private productsService: ProductsService,
     private messageService: MessageService,
@@ -24,6 +28,63 @@ export class DashboardHomeComponent implements OnInit, OnDestroy{
     this.getProductsDatas();
   }
 
+  setProductsChartConfig(): void {
+    if (this.productsList.length > 0) {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+
+  this.productsChartDatas = {
+    labels: this.productsList.map((element) => element?.name ),
+    datasets: [
+      {
+        label: 'Quantidade',
+        backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+        borderColor: documentStyle.getPropertyValue('--indigo-400'),
+        hoverBackgroundColor: documentStyle.getPropertyValue('--indigo-500'),
+        data: this.productsList.map((element) => element?.amount),
+      },
+    ]
+  }
+
+  this.productsChartOptions = {
+    maintainAspectRatio: false,
+    aspectRatio: 0.8,
+    plugins: {
+      legend: {
+        labels: {
+          color: textColor
+        }
+      }
+    },
+
+    scales: {
+      x: {
+        ticks: {
+          color: textColorSecondary,
+          font: {
+            weight: 'normal',
+          },
+        },
+        grid: {
+          color: surfaceBorder
+        }
+      },
+      y: {
+        ticks: {
+          color: textColorSecondary,
+        },
+        grid: {
+          color: surfaceBorder,
+        }
+      }
+    }
+  }
+  }
+}
+
   getProductsDatas(){
     this.productsService
     .getAllProducts()
@@ -33,6 +94,7 @@ export class DashboardHomeComponent implements OnInit, OnDestroy{
         if (response.length > 0) {
           this.productsList = response;
           this.productsDtService.setProductsDatas(this.productsList);
+          this.setProductsChartConfig();
         }
       }, error: (err) => {
         console.log(err);
@@ -45,8 +107,10 @@ export class DashboardHomeComponent implements OnInit, OnDestroy{
       }
     })
   }
+
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
 }
+
