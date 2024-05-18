f26466a970108c431fb7ee35813809c98b00b4a8
marcelledssantos <marcelledossantos2@gmail.com>
2024-05-18 17:30:43 -0300

    add funcionalidade para editar produto

---
+++ Projeto 02 - Stock-Control/stock-control/src/app/models/enums/products/ProductEvent.ts
@@ -0,0 +1,5 @@
+export enum ProductEvent{
+  ADD_PRODUCT_EVENT = 'adicionar Produto',
+  EDIT_PRODUCT_EVENT = 'Editar Produto',
+  SALE_PRODUCT_EVENT = 'Vender Produto',
+}
--- Projeto 02 - Stock-Control/stock-control/src/app/modules/products/components/products-table/products-table.component.ts
+++ Projeto 02 - Stock-Control/stock-control/src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProductEvent } from 'src/app/models/enums/products/ProductEvent';
+import { EventAction } from 'src/app/models/interfaces/products/event/eventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 
 @Component({
@@ -8,6 +10,16 @@ import { GetAllProductsResponse } from 'src/app/models/interfaces/products/respo
 })
 export class ProductsTableComponent {
   @Input() products: Array<GetAllProductsResponse> = [];
+  @Output() productEvent = new EventEmitter<EventAction>();
 
   public productSelected!: GetAllProductsResponse;
+  public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductEvent = ProductEvent.EDIT_PRODUCT_EVENT;
+
+  handleProductEvent(action: string, id?: string): void {
+    if (action && action !== '' ){
+      const productEventData = id && id !== ''? {action, id} : {action};
+      this.productEvent.emit(productEventData);
+    }
+  }
 }
