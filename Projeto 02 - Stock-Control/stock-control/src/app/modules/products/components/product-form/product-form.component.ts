dc2005a3c6d303e202be0b0cc789fa6c0cde16f6
marcelledssantos <marcelledossantos2@gmail.com>
2024-05-18 23:31:59 -0300

    add form para adicionar novo produto

---
+++ Projeto 02 - Stock-Control/stock-control/src/app/modules/products/components/product-form/product-form.component.ts
@@ -0,0 +1,55 @@
+import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/response/getCategoriesResponse';
+
+@Component({
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: []
+})
+export class ProductFormComponent implements OnInit, OnDestroy {
+
+  private readonly destroy$: Subject<void> = new Subject();
+  public categoriesDatas: Array<GetCategoriesResponse> = [];
+  public selectedCategory: Array<{name: string; code: string}> = [];
+  public addProductForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    description: ['', Validators.required],
+    category_id: ['',Validators.required],
+    amount:[0, Validators.required]
+  })
+  constructor(
+    private categoriesService: CategoriesService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private router: Router,
+
+  ){}
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+  getAllCategories(): void {
+    this.categoriesService.getAllCategories()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        if(response.length > 0) {
+          this.categoriesDatas = response;
+        }
+      }
+    })
+  }
+
+  handleSubmitAddProduct(): void {}
+
+  ngOnDestroy(): void {
+   this.destroy$.next();
+   this.destroy$.complete();
+  }
+}
--- Projeto 02 - Stock-Control/stock-control/src/app/modules/products/products.module.ts
+++ Projeto 02 - Stock-Control/stock-control/src/app/modules/products/products.module.ts
@@ -23,11 +23,13 @@ import { RouterModule } from '@angular/router';
 import { PRODUCTS_ROUTES } from './products.routing';
 import { ProductsHomeComponent } from './page/products-home/products-home.component';
 import { ProductsTableComponent } from './components/products-table/products-table.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 @NgModule({
   declarations: [
     ProductsHomeComponent,
-    ProductsTableComponent
+    ProductsTableComponent,
+    ProductFormComponent
   ],
   imports: [
     CommonModule,
