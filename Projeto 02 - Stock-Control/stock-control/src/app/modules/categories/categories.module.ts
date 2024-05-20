eb1cae0375fbb2959cf121c76666db27485dc4d6
marcelledssantos <marcelledossantos2@gmail.com>
2024-05-20 12:43:23 -0300

    add module categories

---
+++ Projeto 02 - Stock-Control/stock-control/src/app/modules/categories/categories.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CATEGORIES_ROUTES } from './categories.routing';
+import { CategoriesHomeComponent } from './page/categories-home/categories-home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TooltipModule } from 'primeng/tooltip';
+import { ConfirmationService } from 'primeng/api';
+
+
+
+@NgModule({
+  declarations: [
+    CategoriesHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(CATEGORIES_ROUTES),
+    SharedModule,
+    HttpClientModule,
+    //PrimeNg
+    CardModule,
+    ButtonModule,
+    TableModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    DynamicDialogModule,
+    DropdownModule,
+    ConfirmDialogModule,
+    TooltipModule
+    ],
+    providers: [DialogService, ConfirmationService],
+})
+export class CategoriesModule { }
